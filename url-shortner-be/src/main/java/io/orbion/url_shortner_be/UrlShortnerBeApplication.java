9a8ffe7c4d2cd046e6f735671401a095e4a96e22
rizalfadiaalfikri <rizalfadiaalfikri859@gmail.com>
2025-01-05 21:10:08 +0700

    initial commit

---
+++ url-shortner-be/src/main/java/io/orbion/url_shortner_be/UrlShortnerBeApplication.java
@@ -0,0 +1,13 @@
+package io.orbion.url_shortner_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortnerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortnerBeApplication.class, args);
+	}
+
+}
---
+++ url-shortner-be/src/test/java/io/orbion/url_shortner_be/UrlShortnerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.orbion.url_shortner_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
