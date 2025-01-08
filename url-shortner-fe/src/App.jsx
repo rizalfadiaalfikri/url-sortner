415dcf6a3f87c7c348f32a53375449a6fb570556
rizalfadiaalfikri <rizalfadiaalfikri859@gmail.com>
2025-01-08 09:52:30 +0700

    feat(fe): create landing page and about page

--- url-shortner-fe/src/App.jsx
+++ url-shortner-fe/src/App.jsx
@@ -1,11 +1,23 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import LandingPage from './components/LandingPage'
+import AboutPage from './components/AboutPage'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
-      <div className='text-red-700'>Hello World</div>
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path='/' element={<LandingPage/>}/>
+          <Route path='/about' element={<AboutPage/>}/>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
     </>
   )
 }
---
+++ url-shortner-fe/src/components/Card.jsx
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+
+function Card({title, desc}) {
+  return (
+    <div
+    //   initial={{ opacity: 0, y: 120 }}
+    //   whileInView={{
+    //     opacity: 1,
+    //     y: 0,
+    //   }}
+    //   viewport={{ once: true }}
+    //   transition={{ duration: 0.5 }}
+      className="shadow-md shadow-slate-400 border flex flex-col px-4 py-8  gap-3 rounded-sm"
+    >
+      <h1 className="text-slate-900 text-xl font-bold ">{title}</h1>
+      <p className="text-slate-700 text-sm"> {desc}</p>
+    </div>
+  );
+}
+
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired,
+};
+
+export default Card;
